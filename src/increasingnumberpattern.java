e2555293c894f6fb67ce703069069803ee4f9fc1
DEVELOPMENT-PATH-2025 <westerncourt4@gmail.com>
2026-07-15 23:19:10 +0530

    Next patterns

--- src/increasingnumberpattern.java
+++ src/increasingnumberpattern.java
@@ -13,4 +13,35 @@ public class increasingnumberpattern {
         }
         sc.close();
     }
+
+    public static class floydtriangle {
+    }
+
+    public static class inversenum {
+        public static void main(String[] args) {
+            int x = 5;
+            for(int i=1;i<=x;i++){
+                for(int j=1;j<=x-i+1;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static class invertedhalfpy {
+        public static void main(String[] args) {
+             int n = 6;
+             for(int i=1;i<=n;i++){
+                 for(int j = 1; j<=n-i;j++){
+                     System.out.print(" ");
+                 }
+                 //inner loop
+                 for(int j=1;j<=i;j++){
+                     System.out.print("* ");
+                 }
+                 System.out.println();
+             }
+        }
+    }
 }
